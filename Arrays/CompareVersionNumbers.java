85cd42306c63160ddf12d4b78ec1ee345cb90c14
K L MANDARA <161995948+KLMANDARA@users.noreply.github.com>
2025-10-02 21:15:24 +0530

    Update CompareVersionNumbers.java

--- Arrays/CompareVersionNumbers.java
+++ Arrays/CompareVersionNumbers.java
@@ -4,7 +4,6 @@ class Solution {
         String[] v2 = version2.split("\\.");
 
         int n = Math.max(v1.length, v2.length);
-
         for (int i = 0; i < n; i++) {
             int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
             int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
