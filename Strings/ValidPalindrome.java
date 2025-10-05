4e1d8ca6a86f56bbe1a52717d8f954a7aa61833a
K L MANDARA <161995948+KLMANDARA@users.noreply.github.com>
2025-10-05 12:05:34 +0530

    Update ValidPalindrome.java

--- Strings/ValidPalindrome.java
+++ Strings/ValidPalindrome.java
@@ -3,7 +3,7 @@ class Solution{
         s=s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
          int n=s.length();
         for(int i=0;i<n/2;i++){
-           
+        
             if(s.charAt(i)!=s.charAt(n-1-i)){
                 return false;
             }
